17d4df4b8dc580b93202a2ccc69b62b5385579e5
FFLAB <riggio.francesco@gmail.com>
2017-02-10 23:53:42 +0100

    Add permalink thumbnails
    Added permalink to post-thumbnails in front-page.php template.

--- front-page.php
+++ front-page.php
@@ -3,14 +3,16 @@
 		<main class="container">
 			<div class="row">
 				<div id="contenuti" class="col-md-12">
-				
+
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 				<div <?php post_class('articolo'); ?>>
 				<h2><a href="<?php the_permalink(); ?> "><?php the_title(); ?></a></h2>
 				<!-- immagine in evidenza -->
-                <?php the_post_thumbnail('full',array('class' => 'img-responsive')); ?>
-           
+                <a href="<?php the_permalink(); ?> ">
+									<?php the_post_thumbnail('full',array('class' => 'img-responsive')); ?>
+								</a>
+
 				<?php the_excerpt() ?>
     	         <a href="<?php the_permalink(); ?>"><span class="readmore">Continua a leggere &raquo;</span></a>
                   <hr>
@@ -25,8 +27,8 @@
                 <?php endif; ?>
 
 <div class="navigation text-center"><?php pagination_nav(); ?></div>
-            
-				</div>	
+
+				</div>
 		</main>
 
-	<?php get_footer(); ?> 	
+	<?php get_footer(); ?>
