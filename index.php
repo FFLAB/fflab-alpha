6fe9da10a0549006bc413e9b2740b2b65e1e3d9b
Francesco Riggio <riggio.francesco@gmail.com>
2018-03-21 22:06:02 +0100

    minor updte

--- index.php
+++ index.php
@@ -3,30 +3,30 @@
 		<main class="container">
 			<div class="row">
 				<div id="contenuti" class="col-md-12">
-					<!--start loop di Wordpress -->
+					<!--start Wordpress loop -->
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 				<div <?php post_class('articolo'); ?>>
 
 				<h2><a href="<?php the_permalink(); ?> "><?php the_title(); ?></a></h2>
 				<?php the_content() ?>
-            <p><small>Scritto da <strong><?php the_author() ?></strong> il <?php the_time('d F Y'); ?></small></p>
+            	<p><small>Scritto da <strong><?php the_author() ?></strong> il <?php the_time('d F Y'); ?></small></p>
 				<?php the_tags(
-                   "<p><small>Argomenti: ", //prima dei tags
-                    ", ",   //separatore dei tags
-                    "</small></p>"  //dopo i tags
+                   "<p><small>Argomenti: ", 
+                    ", ",   
+                    "</small></p>"  
                     ); ?>
 				
 				</div>
 				<?php endwhile; else: ?>
-<!-- alternative text-->
+				<!-- alternative text-->
                 <div class="articolo">
                     <h2>Attenzione!</h2>
                     <p>Nessun elemento trovato</p>
                 </div>
                 <?php endif; ?>
                 <hr>
-<!--    end loop Wordpress -->
+				<!--    end loop Wordpress -->
 
 				</div>
 				
